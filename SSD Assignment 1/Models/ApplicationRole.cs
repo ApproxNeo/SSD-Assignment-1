10882c477fe30280001ee2d1c68b71b62ac1fbdd
s10205832@connect.np.edu.sg <s10205832@connect.np.edu.sg>
2021-06-23 20:58:37 +0800

    Updated the ApplicationRole class

--- SSD Assignment 1/Models/ApplicationRole.cs
+++ SSD Assignment 1/Models/ApplicationRole.cs
@@ -1,11 +1,16 @@
 ﻿using System;
+using Microsoft.AspNetCore.Identity;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace SSD_Assignment_1.Models
 {
-    public class ApplicationRole
+    public class ApplicationRole: IdentityRole
     {
+        public string Description { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public string IPAddress { get; set; }
+
     }
 }
