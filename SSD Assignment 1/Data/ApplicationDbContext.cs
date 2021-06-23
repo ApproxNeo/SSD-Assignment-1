46f41318cc8b30694f8fee508eaada6060bacdc2
Clement <Clementnjk@gmail.com>
2021-06-23 15:27:33 +0800

    Add project files.

---
+++ SSD Assignment 1/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace SSD_Assignment_1.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
