a5c6baed15854b06dde3217ef4a7f9d09d885436
s10205832@connect.np.edu.sg <s10205832@connect.np.edu.sg>
2021-06-23 20:48:19 +0800

    I have created a folder named models.The folder is used to save our classes and added a class name for ApplicationRole.cs which is used for RBAC.

---
+++ SSD Assignment 1/Models/ApplicationRole.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SSD_Assignment_1.Models
+{
+    public class ApplicationRole
+    {
+    }
+}
