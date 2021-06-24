d18b3256857e6de90ea4c843a52961a95ccad6bd
s10205832@connect.np.edu.sg <s10205832@connect.np.edu.sg>
2021-06-25 03:02:32 +0800

    Created a class named ApplicationUser under models.I updated the SSD_Assignment_1Context.cs by updating the parameters.

--- SSD Assignment 1/Data/SSD_Assignment_1Context.cs
+++ SSD Assignment 1/Data/SSD_Assignment_1Context.cs
@@ -2,17 +2,28 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+
 using SSD_Assignment_1.Models;
 
 namespace SSD_Assignment_1.Data
 {
-    public class SSD_Assignment_1Context : DbContext
+
+    public class SSD_Assignment_1Context : IdentityDbContext<ApplicationUser, ApplicationRole, string>
     {
         public SSD_Assignment_1Context (DbContextOptions<SSD_Assignment_1Context> options)
             : base(options)
         {
         }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            // Customize the ASP.NET Identity model and override the defaults if needed.
+            // For example, you can rename the ASP.NET Identity table names and more.
+            // Add your customizations after calling base.OnModelCreating(builder);
+        }
+
 
         public DbSet<SSD_Assignment_1.Models.Product> Product { get; set; }
     }
---
+++ SSD Assignment 1/Models/ApplicationUser.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.AspNetCore.Identity;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SSD_Assignment_1.Models
+{
+    public class ApplicationUser:IdentityUser
+    {
+        public string FullName { get; set; }
+        public DateTime BirthDate { get; set; }
+        public int Age { get; set; }
+
+    }
+}
