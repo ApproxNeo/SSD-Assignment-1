428f744d3c3ebb76d6695d7f708ab9a4715a9fdc
s10205832@connect.np.edu.sg <s10205832@connect.np.edu.sg>
2021-06-25 05:46:08 +0800

    I have created a folder named Roles under Pages and have created the Create,delete and so on.

---
+++ SSD Assignment 1/Pages/Roles/Delete.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using SSD_Assignment_1.Models;
+using System;
+using System.Threading.Tasks;
+
+namespace SSD_Assignment_1.Pages.Roles
+{
+    public class DeleteModel : PageModel
+    {
+
+    }
+}
