f66e1f32a4287d2b7d4dd675564400da95301201
s10205832@connect.np.edu.sg <s10205832@connect.np.edu.sg>
2021-06-27 16:43:59 +0800

    Added authorization for index page and privacy page

--- SSD Assignment 1/Pages/Privacy.cshtml.cs
+++ SSD Assignment 1/Pages/Privacy.cshtml.cs
@@ -2,12 +2,14 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
 
 namespace SSD_Assignment_1.Pages
 {
+    [Authorize(Roles = "Admin,Product manager")]
     public class PrivacyModel : PageModel
     {
         private readonly ILogger<PrivacyModel> _logger;
