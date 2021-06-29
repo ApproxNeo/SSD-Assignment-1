adc1d213095ab97ab2c93fa1fa585451e1ec38ea
s10205832@connect.np.edu.sg <s10205832@connect.np.edu.sg>
2021-06-29 10:57:39 +0800

    Updated the privacy page

--- SSD Assignment 1/Pages/Privacy.cshtml.cs
+++ SSD Assignment 1/Pages/Privacy.cshtml.cs
@@ -9,7 +9,7 @@ using Microsoft.Extensions.Logging;
 
 namespace SSD_Assignment_1.Pages
 {
-    [Authorize(Roles = "Admin,Product manager")]
+    [AllowAnonymous]
     public class PrivacyModel : PageModel
     {
         private readonly ILogger<PrivacyModel> _logger;
