6ca8086198de4970b0db5d4b53d3ad86988efd95
s10205832@connect.np.edu.sg <s10205832@connect.np.edu.sg>
2021-07-03 22:26:50 +0800

    Configured error page...

--- SSD Assignment 1/Pages/Error.cshtml.cs
+++ SSD Assignment 1/Pages/Error.cshtml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
@@ -13,6 +14,9 @@ namespace SSD_Assignment_1.Pages
     public class ErrorModel : PageModel
     {
         public string RequestId { get; set; }
+        public int iStatusCode { get; set; }
+        public string Message { get; set; }
+        public string StackTrace { get; set; }
 
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
 
@@ -26,6 +30,13 @@ namespace SSD_Assignment_1.Pages
         public void OnGet()
         {
             RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier;
+            // Get the details of the exception that occurred
+            var exception = HttpContext.Features.Get<IExceptionHandlerFeature>();
+
+            iStatusCode = HttpContext.Response.StatusCode;
+            Message = exception.Error.Message;
+            StackTrace = exception.Error.StackTrace;
+
         }
     }
 }
