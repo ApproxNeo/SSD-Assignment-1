8760c14e787bb912be49c718b83b4e78883333e1
Isha <s10205038@connect.np.edu.sg>
2021-07-06 23:10:20 +0800

    added birthdate field to registration

--- SSD Assignment 1/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ SSD Assignment 1/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -61,6 +61,11 @@ namespace SSD_Assignment_1.Areas.Identity.Pages.Account
             [Display(Name = "Confirm password")]
             [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
             public string ConfirmPassword { get; set; }
+
+            [Required]
+            [Display(Name = "Birth Date")]
+            [DataType(DataType.Date)]
+            public DateTime BirthDate { get; set; }
         }
 
         public async Task OnGetAsync(string returnUrl = null)
@@ -75,7 +80,7 @@ namespace SSD_Assignment_1.Areas.Identity.Pages.Account
             ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
             if (ModelState.IsValid)
             {
-                var user = new ApplicationUser { UserName = Input.Email, Email = Input.Email };
+                var user = new ApplicationUser { UserName = Input.Email, Email = Input.Email, BirthDate = Input.BirthDate };
                 var result = await _userManager.CreateAsync(user, Input.Password);
                 if (result.Succeeded)
                 {
