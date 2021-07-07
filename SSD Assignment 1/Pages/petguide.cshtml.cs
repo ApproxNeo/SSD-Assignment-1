03836f0515df1dd382a7bb0026aba9d2ee7952d6
s10205832@connect.np.edu.sg <s10205832@connect.np.edu.sg>
2021-07-07 20:00:16 +0800

    Completed the page which will tell us ways to take care of pets

---
+++ SSD Assignment 1/Pages/petguide.cshtml.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SSD_Assignment_1.Pages
+{
+    public class petguideModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
