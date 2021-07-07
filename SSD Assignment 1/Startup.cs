3258afbab1bf1a0c9465c96962c5095124abd7dc
s10205832@connect.np.edu.sg <s10205832@connect.np.edu.sg>
2021-07-07 10:57:36 +0800

    Uodated the nav bar

--- SSD Assignment 1/Startup.cs
+++ SSD Assignment 1/Startup.cs
@@ -55,7 +55,6 @@ namespace SSD_Assignment_1
             services.AddMvc()
             .AddRazorPagesOptions(options =>
             {
-                options.Conventions.AuthorizeFolder("/Admin");
                 options.Conventions.AuthorizeFolder("/Admin/Products");
                 options.Conventions.AuthorizeFolder("/Admin/Roles");
                 options.Conventions.AuthorizeFolder("/Admin/Audit");
