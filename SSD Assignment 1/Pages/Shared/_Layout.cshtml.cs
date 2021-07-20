3bf1ba15fd11746751c01bb1fe14d64cb93087ae
Clement <Clementnjk@gmail.com>
2021-07-20 17:30:34 +0800

    Navbar Search + QOL changes

---
+++ SSD Assignment 1/Pages/Shared/_Layout.cshtml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SSD_Assignment_1.Pages.Shared
+{
+    public class Layout
+    {
+
+        private readonly SSD_Assignment_1.Data.SSD_Assignment_1Context _context;
+
+        public Layout(SSD_Assignment_1.Data.SSD_Assignment_1Context context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public string SearchString { get; set; }
+    }
+}
