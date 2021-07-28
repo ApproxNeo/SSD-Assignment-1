d6319e75098680561985a34525a9f2e8e1d8be8d
s10205832@connect.np.edu.sg <s10205832@connect.np.edu.sg>
2021-07-29 00:07:41 +0800

    Error in audit for roles

--- SSD Assignment 1/Migrations/20210728160429_AddAuditRole.Designer.cs
+++ SSD Assignment 1/Migrations/20210728160429_AddAuditRole.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SSD_Assignment_1.Data;
 
 namespace SSD_Assignment_1.Migrations
 {
     [DbContext(typeof(SSD_Assignment_1Context))]
-    partial class SSD_Assignment_1ContextModelSnapshot : ModelSnapshot
+    [Migration("20210728160429_AddAuditRole")]
+    partial class AddAuditRole
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
