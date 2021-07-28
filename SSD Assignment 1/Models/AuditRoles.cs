c1a1f3b1b482502294165b81d4cac0224b4b53da
s10205832@connect.np.edu.sg <s10205832@connect.np.edu.sg>
2021-07-29 00:07:41 +0800

    Error in audit for roles

---
+++ SSD Assignment 1/Models/AuditRoles.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SSD_Assignment_1.Models
+{
+    public class AuditRoles
+    {
+
+        [Key]
+        public int Audit_ID { get; set; }
+
+        [Display(Name = "Audit Action")]
+        public string AuditActionType { get; set; }
+ 
+
+        [Display(Name = "Performed By")]
+        public string PerformedById { get; set; }
+
+        [Display(Name = "Performed On ")]
+        public string PerformOnId { get; set; }
+
+        [Display(Name = "Date/Time Stamp")]
+        [DataType(DataType.DateTime)]
+        public DateTime DateTimeStamp { get; set; }
+  
+
+        [Display(Name = "Role Record ID ")]
+        public int KeyRoleFieldID { get; set; }
+
+
+
+
+    }
+}
