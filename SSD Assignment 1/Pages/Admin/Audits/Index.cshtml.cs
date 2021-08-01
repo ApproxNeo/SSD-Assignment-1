60c9af3bda4bd03f308ba3db775411f3655a0133
s10205832@connect.np.edu.sg <s10205832@connect.np.edu.sg>
2021-08-01 12:26:04 +0800

    Created the email message that will be sent when logged.Dont forget to change the email and password ...Will probably create a real email address later...

--- SSD Assignment 1/Pages/Admin/Audits/Index.cshtml.cs
+++ SSD Assignment 1/Pages/Admin/Audits/Index.cshtml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Mail;
+using System.Text;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
@@ -9,6 +11,7 @@ using Microsoft.EntityFrameworkCore;
 using Newtonsoft.Json;
 using SSD_Assignment_1.Data;
 using SSD_Assignment_1.Models;
+using SSD_Assignment_1.Pages.Admin.Audits;
 
 namespace SSD_Assignment_1.Pages.Admin.Audits
 {
@@ -20,6 +23,12 @@ namespace SSD_Assignment_1.Pages.Admin.Audits
     {
         private readonly SSD_Assignment_1.Data.SSD_Assignment_1Context _context;
 
+        [BindProperty]
+        public string EMAIL { get; set; }
+
+        [BindProperty]
+        public string PASSWORD { get; set; }
+
         public IndexModel(SSD_Assignment_1.Data.SSD_Assignment_1Context context)
         {
             _context = context;
@@ -30,6 +39,36 @@ namespace SSD_Assignment_1.Pages.Admin.Audits
         public async Task OnGetAsync()
         {
             Audit = await _context.AuditLogs.ToListAsync();
+            if (DateTime.Now.TimeOfDay == new TimeSpan(0, 0, 0))
+            {
+                EMAIL = Environment.GetEnvironmentVariable("EMAIL");
+                PASSWORD = Environment.GetEnvironmentVariable("PASSWORD");
+                string to = EMAIL; //To address    
+                string from = EMAIL; //From address    
+                MailMessage message = new MailMessage(from, to);
+
+                string mailbody = "Dear Admin," +
+                    "Update in roles are assigned.Please take a look at it.Thank you! ";
+                message.Subject = "Update on role assigned";
+                message.Body = mailbody;
+                message.BodyEncoding = Encoding.UTF8;
+                message.IsBodyHtml = true;
+                SmtpClient client = new SmtpClient("smtp.outlook.com", 587); //Gmail smtp    
+                System.Net.NetworkCredential basicCredential1 = new
+                System.Net.NetworkCredential(EMAIL, PASSWORD);
+                client.EnableSsl = true;
+                client.UseDefaultCredentials = false;
+                client.Credentials = basicCredential1;
+                try
+                {
+                    client.Send(message);
+                }
+
+                catch (Exception ex)
+                {
+                    throw ex;
+                }
+            }
         }
 
         public string DictParser (string dict)
--- SSD Assignment 1/Pages/Admin/Audits/RoleAudit.cshtml.cs
+++ SSD Assignment 1/Pages/Admin/Audits/RoleAudit.cshtml.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Mail;
+using System.Text;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
@@ -18,6 +20,11 @@ namespace SSD_Assignment_1.Pages.Admin.Audits.RoleAudit
         private readonly SSD_Assignment_1.Data.SSD_Assignment_1Context _context;
         private readonly UserManager<ApplicationUser> _userManager;
 
+        [BindProperty]
+        public string EMAIL { get; set; }
+
+        [BindProperty]
+        public string PASSWORD { get; set; }
         public RoleAuditModel(SSD_Assignment_1.Data.SSD_Assignment_1Context context, UserManager<ApplicationUser> userManager)
         {
             _context = context;
@@ -29,6 +36,36 @@ namespace SSD_Assignment_1.Pages.Admin.Audits.RoleAudit
         public async Task OnGetAsync()
         {
             Audits = await _context.RoleAuditRecord.ToListAsync();
+            if (DateTime.Now.TimeOfDay == new TimeSpan(0, 0, 0))
+            {
+                EMAIL = Environment.GetEnvironmentVariable("EMAIL");
+                PASSWORD = Environment.GetEnvironmentVariable("PASSWORD");
+                string to = EMAIL; //To address    
+                string from = EMAIL; //From address    
+                MailMessage message = new MailMessage(from, to);
+
+                string mailbody = "Dear Admin," +
+                    "Update in roles are assigned.Please take a look at it.Thank you! ";
+                message.Subject = "Update on role assigned";
+                message.Body = mailbody;
+                message.BodyEncoding = Encoding.UTF8;
+                message.IsBodyHtml = true;
+                SmtpClient client = new SmtpClient("smtp.outlook.com", 587); //Gmail smtp    
+                System.Net.NetworkCredential basicCredential1 = new
+                System.Net.NetworkCredential(EMAIL, PASSWORD);
+                client.EnableSsl = true;
+                client.UseDefaultCredentials = false;
+                client.Credentials = basicCredential1;
+                try
+                {
+                    client.Send(message);
+                }
+
+                catch (Exception ex)
+                {
+                    throw ex;
+                }
+            }
         }
     }
 }
