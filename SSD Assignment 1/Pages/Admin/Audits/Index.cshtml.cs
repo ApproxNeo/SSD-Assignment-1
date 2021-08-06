ac18ebc50ed0d5a60d6733d897f28fdc808fdfec
s10205832@connect.np.edu.sg <s10205832@connect.np.edu.sg>
2021-08-06 08:05:14 +0800

    Forgot to add permission for audit roles...

--- SSD Assignment 1/Pages/Admin/Audits/Index.cshtml.cs
+++ SSD Assignment 1/Pages/Admin/Audits/Index.cshtml.cs
@@ -13,9 +13,6 @@ using SSD_Assignment_1.Models;
 namespace SSD_Assignment_1.Pages.Admin.Audits
 {
     [Authorize(Roles = "Business Owner,Admin")]
-    //couldnt get "Product manager" role to work at all,
-    //thinking its cause its 2 words, "prodMngr" works fine for me,
-    //need someone help see if they experience same issue - Clement
     public class IndexModel : PageModel
     {
         private readonly SSD_Assignment_1.Data.SSD_Assignment_1Context _context;
