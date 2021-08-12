19bf151e0bb2cfce91b0ff2fd39e1264f74a2f7d
Clement <Clementnjk@gmail.com>
2021-08-12 10:30:57 +0800

    CSS Changes

---
+++ SSD Assignment 1/Pages/AboutUs.cshtml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AspNetCoreHero.ToastNotification.Abstractions;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using SSD_Assignment_1.Models;
+
+namespace SSD_Assignment_1.Pages
+{
+    [AllowAnonymous] 
+    public class AboutUsModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+        private readonly INotyfService _notyf;
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+
+        public AboutUsModel(ILogger<IndexModel> logger, INotyfService notyf, UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager)
+        {
+            _logger = logger;
+            _notyf = notyf;
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+        public async Task<IActionResult> OnGet()
+        {
+
+            
+
+            return Page();
+        }
+    }
+}
